2d7c248e51f0deab4864d1b2b68fc6d504e10ae8
Sayyid Zavic <msayyidsyafiq@gmail.com>
2025-07-06 16:26:22 +0700

    fix layout, SEO, navbar, pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  site: 'https://www.tunetuf.com',
+  integrations: [sitemap()]
 });
